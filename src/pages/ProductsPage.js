23bd415a013e65c899be2a8b98a3bee3a9211f3f
icchigoo <icchigoo12@gmail.om>
2023-08-01 22:59:51 +0545

    Whole page clickable

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -8,12 +8,13 @@ import {
   Typography,
   Card,
   Box,
-  Link as MuiLink,
+
   Stack,
   Button,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Iconify from "../components/iconify/Iconify";
+import { Link as RouterLink } from "react-router-dom";
 
 import { Grid } from "@mui/material";
 
@@ -51,55 +52,34 @@ const PropertyList = () => {
 
   return (
     <>
-
       <Container>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          mb={5}
-        >
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Property
           </Typography>
-          <Button
-          component={Link}
-          to="/dashboard/property"
-          variant="contained"
-          startIcon={<Iconify icon="eva:edit-fill" />}
-        >
-          Add Property
-        </Button>
+          <Button component={Link} to="/dashboard/property" variant="contained" startIcon={<Iconify icon="eva:edit-fill" />}>
+            Add Property
+          </Button>
         </Stack>
 
         <Grid container spacing={3}>
           {propertyState.map((property, index) => (
             <Grid key={index} item xs={12} sm={6} md={6}>
-              <Card>
-                <Box sx={{ pt: imageHeight, position: "relative" }}>
-                  <StyledProductImg
-                    alt={property.name}
-                    src={property.images[selectedImageIndex]?.url}
-                  />
-                </Box>
-
-                <Stack spacing={2} sx={{ p: 3 }}>
-                  <MuiLink
-                    component={Link}
-                    to={`/admin/card-details/${property._id}`}
-                    color="inherit"
-                    underline="hover"
-                  >
+              <RouterLink to={`/admin/card-details/${property._id}`} style={{ textDecoration: "none" }}>
+                <Card>
+                  <Box sx={{ pt: imageHeight, position: "relative" }}>
+                    <StyledProductImg alt={property.name} src={property.images[selectedImageIndex]?.url} />
+                  </Box>
+                  <Stack spacing={2} sx={{ p: 3 }}>
                     <Typography variant="subtitle2" noWrap>
                       {property.name || "No Name"}
                     </Typography>
-                  </MuiLink>
-
-                  <Typography variant="subtitle1" noWrap>
-                    {property.address || "No Address"}
-                  </Typography>
-                </Stack>
-              </Card>
+                    <Typography variant="subtitle1" noWrap>
+                      {property.address || "No Address"}
+                    </Typography>
+                  </Stack>
+                </Card>
+              </RouterLink>
             </Grid>
           ))}
         </Grid>
