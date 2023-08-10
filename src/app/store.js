80cf344add4d928696186992fe745734d41b2f4d
icchigoo <icchigoo12@gmail.om>
2023-08-10 22:04:57 +0545

    Removed customer

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import customerReducer from "../features/cutomers/customerSlice";
 import uploadReducer from "../features/upload/uploadSlice";
 import transactionReducer from "../features/transaction/transactionSlice";
 import expensesReducer from "../features/expenses/expensesSlice";
@@ -9,7 +8,6 @@ import incomesReducer from "../features/incomes/incomesSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    customer: customerReducer,
     upload: uploadReducer,
     transaction: transactionReducer,
     expenses: expensesReducer,
