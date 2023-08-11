7f7617d1480ce29559f26f7211fb00b43d5fa14a
icchigoo <icchigoo12@gmail.om>
2023-08-11 14:18:31 +0545

    merged

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,22 +1,25 @@
 import axios from "axios";
-import { config } from "./config";
 
-// Create an instance of axios
 const instance = axios.create();
 
 // Add an interceptor to modify the request before it's sent
 instance.interceptors.request.use(
-  // The first function is called before the request is sent
   (axiosConfig) => {
-    // Add authentication headers from the "config" object
-    axiosConfig.headers = config.headers; // Here, "config" contains your authorization headers
-    return axiosConfig; // Return the modified request configuration
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    // Add authentication headers if user is logged in
+    if (user && user.token) {
+      axiosConfig.headers.Authorization = `Bearer ${user.token}`;
+    }
+
+    // Always set the Accept header for JSON responses
+    axiosConfig.headers.Accept = "application/json";
+
+    return axiosConfig;
   },
-  // The second function handles errors that occur during the request
   (error) => {
-    return Promise.reject(error); // Reject the promise with the error
+    return Promise.reject(error);
   }
 );
 
-// Export the customized axios instance
 export default instance;
